b49e43b4b96f21d839e1faf9af53c9dbb3b3fee4
Michael Gutierrez <13262904+iamgutz@users.noreply.github.com>
2023-04-14 18:20:37 -0600

    changed out folder name and added config file for svgr

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Flat from './flat';
-export * as FlatRounded from './flat-rounded';
-export * as Logo from './logo';
-export * as Mono from './mono';
-export * as MonoOutline from './mono-outline';
